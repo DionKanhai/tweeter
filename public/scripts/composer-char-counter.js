172228c64f4bca48a3bc6e4fa9a8a79309d145f8
DionKanhai <dionkanhais@gmail.com>
2022-10-06 01:19:22 +0000

    implemented tweeter counter, tweeter styling for counter and add html with css for user tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  //this refers to the textarea input
+  $("#tweet-text").on('input', function() {
+    // counts the individual num of chars entered in textarea
+    let charLength = $(this).val().length
+    // grab the counter using the textarea position in the DOM
+  const counter = $(this).next().children()[1]
+  //set value of counter
+  counter.innerHTML = 140 - charLength;
+  // apply red color if value in textarea exceeds 140 limit
+  if (140 - charLength < 0) {
+    $("output").addClass("when-negative");
+  }
+  else {
+    $("output").removeClass("when-negative");
+  }
+  });
+});
